0c0c660cb8b9449e414556c3a0a5c219bdef5b84
sungp <aki18082002@gmail.com>
2025-02-06 01:19:41 +0700

    Add 2 problem

---
+++ inorderTraversal.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+ * right(right) {}
+ * };
+ */
+class Solution {
+public:
+  void inorder(TreeNode *root, vector<int> &result) {
+    if (!root)
+      return;
+    inorder(root->left, result);  // Visit left subtree
+    result.push_back(root->val);  // Visit root
+    inorder(root->right, result); // Visit right subtree
+  }
+
+  vector<int> inorderTraversal(TreeNode *root) {
+    vector<int> result;
+    inorder(root, result);
+    return result;
+  }
+};
